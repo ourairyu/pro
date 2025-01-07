f1b6a1da07198434eb93446c22b638755a834d8f
Ourai Lin <ourairyu@gmail.com>
2025-01-07 09:31:56 +0800

    chore: initialize repo

---
+++ .knosys/sites/default/_assets/javascripts/statistic.js
@@ -0,0 +1,10 @@
+/**
+ * 百度统计
+ */
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?6a882330b150fbc388356515ef87add5";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ README.md
@@ -0,0 +1,3 @@
+# Pro
+
+Ourai's English personal website.
